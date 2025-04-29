4649d5ffad65dc2e00145fb9cb06919f0e2e10e5
王俊邦 <st10202095@gmail.com>
2025-04-29 22:59:44 +0800

    第一次上傳

---
+++ 四則運算(switch case).c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+	double n1,n2;
+	char op;
+	printf("Enter two number\nFirst number:");
+	scanf("%lf",&n1);
+	printf("Second number:");
+	scanf("%lf",&n2);
+    printf("Enter:+,-,*,/):");
+    scanf(" %c",&op);
+    switch(op)
+    {
+    	case '+':
+    		printf("%.1lf + %.1lf = %.1lf",n1,n2,n1 + n2);
+    		break;
+    	case '-':
+    		printf("%.1lf - %.1lf = %.1lf",n1,n2,n1 - n2);
+    		break;
+    	case '*':
+    		printf("%.1lf * %.1lf = %.1lf",n1,n2,n1 * n2);
+    		break;
+		case '/':
+		    printf("%.1lf / %.1lf = %.1lf",n1,n2,n1 / n2);
+			break;	
+	}
+}  
